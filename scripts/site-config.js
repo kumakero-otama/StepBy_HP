4b9622bd628612d278f3b41c70bdefa82afeb3a5
kumakero-otama <kumakero.otama@gmail.com>
2026-05-03 14:58:46 +0900

    v1.4.2 ・点字ブロックランキングがうまく表示されないバグを直した（はず）

--- scripts/site-config.js
+++ scripts/site-config.js
@@ -1,7 +1,7 @@
 window.StepBySiteConfig = {
-  version: "HP: v1.4.1",
+  version: "HP: v1.4.2",
   api: {
-    tactileRanking: "/api/tactile-ranking"
+    tactileRanking: "https://barrierfree-map.loophole.site/api/tactile-ranking"
   },
   links: {
     mapApp: "https://kumakero-otama.github.io/StepBy/UI2/map/Index.html"
