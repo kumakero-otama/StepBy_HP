647a7a63d5510b23cddedf3e7a3041700ab98034
kumakero-otama <kumakero.otama@gmail.com>
2026-07-05 21:07:22 +0900

    v1.8.3 ・使い方セクションのすべての要素を入れた

--- scripts/site-config.js
+++ scripts/site-config.js
@@ -1,5 +1,5 @@
 window.StepBySiteConfig = {
-  version: "HP: v1.8.2",
+  version: "HP: v1.8.3",
   api: {
     stats: "https://barrierfree-map.tail5de5e1.ts.net/api/stats",
     tactileRanking: "https://barrierfree-map.tail5de5e1.ts.net/api/tactile-ranking",
